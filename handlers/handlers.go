8fa5318f4f248ddc71982b8b5c7b80640324f6f7
ShinnosukeSuzuki <bruno0821ss@gmail.com>
2024-07-08 22:51:50 +0900

    commit 1-4

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,51 +7,27 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		http.Error(w, "Inavlid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Hello, world!\n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article...\n")
-	} else {
-		http.Error(w, "Inavlid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Article...\n")
 }
 
 func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
-	} else {
-		http.Error(w, "Inavlid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article List\n")
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%d\n", articleID)
-		io.WriteString(w, resString)
-	} else {
-		http.Error(w, "Inavlid method", http.StatusMethodNotAllowed)
-	}
+	articleID := 1
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
 }
 
 func PostNiceHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		io.WriteString(w, "Postig Nice...\n")
-	} else {
-		http.Error(w, "Inavlid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Postig Nice...\n")
 }
 
 func PostCommentHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		io.WriteString(w, "Posting Comment...\n")
-	} else {
-		http.Error(w, "Inavlid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Comment...\n")
 }
