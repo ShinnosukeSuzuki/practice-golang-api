390965ff84187c289e3e0f5279818d98c2ce1cfa
ShinnosukeSuzuki <bruno0821ss@gmail.com>
2024-07-09 22:47:54 +0900

    commit 2-6

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 
+	"github.com/ShinnosukeSuzuki/practice-golang-api/models"
 	"github.com/gorilla/mux"
 )
 
@@ -14,7 +17,31 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "Invalid Content-Length", http.StatusBadRequest)
+		return
+	}
+
+	reqBodyBuffer := make([]byte, length)
+	if _, err := r.Body.Read(reqBodyBuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "Invalid to get request body\n", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodyBuffer, &reqArticle); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	jsonData, err := json.Marshal(reqArticle)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
 
 func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,24 +59,47 @@ func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
 		page = 1
 	}
 
-	resString := fmt.Sprintf("Article List (page: %d)\n", page)
-	io.WriteString(w, resString)
+	articleList := []models.Article{models.Article1, models.Article2}
+	jsonData, err := json.Marshal(articleList)
+	if err != nil {
+		errMsg := fmt.Sprintf("fail to encode json (page %d)\n", page)
+		http.Error(w, errMsg, http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, r *http.Request) {
-	articleID, err := strconv.Atoi(mux.Vars(r)["id"])
+	_, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		http.Error(w, "Invalid article ID", http.StatusBadRequest)
 		return
 	}
-	resString := fmt.Sprintf("Article No.%d\n", articleID)
-	io.WriteString(w, resString)
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
 
 func PostNiceHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Postig Nice...\n")
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
 
 func PostCommentHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	comment := models.Comment1
+	jsonData, err := json.Marshal(comment)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
