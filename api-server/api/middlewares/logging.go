9c0cee10eebffe2a3db5d71c4842a8ce6f1f9e8d
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-07-15 17:10:28 +0900

    commit 9-3

--- api-server/api/middlewares/logging.go
+++ api-server/api/middlewares/logging.go
@@ -5,10 +5,35 @@ import (
 	"net/http"
 )
 
+// 自作ResponseWriterを作成
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+// コンストラクトを作る
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{w, http.StatusOK}
+}
+
+// WriteHeaderをオーバーライド
+func (w *resLoggingWriter) WriteHeader(code int) {
+	w.code = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(r.URL.Path, r.Method)
-		next.ServeHTTP(w, r)
+
+		// 自作のResponseWriterを作成
+		rlw := NewResLoggingWriter(w)
+
+		// ハンドラに渡す
+		next.ServeHTTP(rlw, r)
+
+		// 自作ResponseWriterからロギングしたいデータを出す
+		log.Println("res: ", rlw.code)
 	})
 }
